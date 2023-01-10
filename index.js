3292d16bb37e864bba8a4cfa0819ce2202008b40
Julie M <seejulielee@gmail.com>
2023-01-10 15:55:50 -0800

    Set up base layout

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Remember to import the data and Dog class!
+// import {dogs} from 'data.js'
+
+// const container = document.querySelector("#container")
+
+// container.innerHTML = 
+// `<div class="top-nav">
+//     <img src="./images/icon-profile.png" alt="profile-icon"/>
+//     <img src="./images/logo.png" alt="logo-icon"/>
+//     <img src="./images/chat-profile.png" alt="chat-icon"/>
+// </div>
+// <div class="dog">
+//     <img src="" alt="dog-image">
+//     <h3>NAME</h3>
+//     <p>TAGLINE</p>
+
+// </div>
+// <div class="bottom-buttons">
+//     <i></i>
+//     <i></i>
+// </div>
+// `
+
+
+
