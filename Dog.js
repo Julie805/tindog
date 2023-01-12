0423bba4f4f0956c27bd10e9ca463aded4e9ed57
Julie M <seejulielee@gmail.com>
2023-01-12 12:54:30 -0800

    Added like and unlike buttons

--- Dog.js
+++ Dog.js
@@ -3,14 +3,20 @@ class Dog {
     Object.assign(this, data)
   }
   
+  //Badges are set to display none until a button is clicked
   getDogHtml () {
     const {avatar, name, age, bio} = this
     return `
       <img class="dog-img" src="${avatar}" alt="dog-image">
+      <img class="like-badge" src= "./images/badge-like.png" alt="like-badge" />
+      <img class="nope-badge" src= "./images/badge-nope.png" alt="nope-badge" />
       <h3 class="dog-id">${name}, ${age}</h3>
-      <p class="dog-bio">${bio}</p>  
+      <p class="dog-bio">${bio}</p>
       `
    } 
+
+   
 }
+
 
 export default Dog
--- index.js
+++ index.js
@@ -4,25 +4,42 @@
 
 // dogArray is set up so that it can be manipulated. Is there a way not to have this hard-coded though?
 
- let dogArray = dogs
-
-// pulls out the first dog object available from dogsArray or renders a new one
-
+let dogArray = dogs
+// dog works to render first dog, but would be better to have a getNewDog() as its value, and that function finds both the first dog and all the other dogs.
 const dog = new Dog(dogs[0])
 
+ //currently unused
  function getNewDog() {
   const nextDogData = dogArray.shift()
   return nextDogData 
  }
  //  new Dog(nextDogData) : {}
 
+ document.querySelector('#heart').addEventListener('click', function () {
+  likeDog()
+ })
 
- function renderDog() {
+ document.querySelector('#cross').addEventListener('click', function () {
+  unlikeDog() 
+ })
+
+ function likeDog () {
+  document.querySelector('.like-badge').style.display = "block"
+  document.querySelector('.nope-badge').style.display = "none"
+ }
+
+ function unlikeDog () {
+  document.querySelector('.nope-badge').style.display = "block"
+  document.querySelector('.like-badge').style.display = "none"
+ }
+
+
+ function render() {
   document.querySelector('.dog').innerHTML = dog.getDogHtml()
  }
 
 //  let dog = getNewDog()
-renderDog()
+render()
  
 
 
